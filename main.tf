8864c962a4b6af11787fb8316b5488deaf9bc1ec
louismichel100 <sangalamballamichel@gmail.com>
2024-03-12 06:48:38 +0000

    next lab

--- main.tf
+++ main.tf
@@ -1,25 +1,19 @@
-#Create and bootstrap webserver
-resource "aws_instance" "webserver" {
-  ami                         = data.aws_ssm_parameter.webserver-ami.value
-  instance_type               = "t3.micro"
-  key_name                    = aws_key_pair.webserver-key.key_name
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.sg.id]
-  subnet_id                   = aws_subnet.subnet.id
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum -y install httpd && sudo systemctl start httpd",
-      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
-      "sudo mv index.html /var/www/html/"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
-  }
-  tags = {
-    Name = "webserver"
-  }
+variable "main_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+provider "aws" {
+  region = var.main_region
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  region = var.main_region
+}
+
+resource "aws_instance" "my-instance" {
+  ami           = module.vpc.ami_id
+  subnet_id     = module.vpc.subnet_id
+  instance_type = "t2.micro"
 }
